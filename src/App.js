746dbb3ffd52c39ccb44073b7af01093633bfdeb
sabirul-islam <sabirul.shimul@gmail.com>
2020-09-11 21:58:14 +0600

    shipment complete

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
   const [loggedInUser, setLoggedInUser] = useState({});
   return (
     <userContext.Provider value={[loggedInUser, setLoggedInUser]}>
-      <h3>email: {loggedInUser.email}</h3>
       
       <Router>
       <Header></Header>
